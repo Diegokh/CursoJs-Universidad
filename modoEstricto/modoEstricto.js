6d83d517c264916f5e6d458904b72ca847ec232f
Diegokh <diegoblanque04@outlook.com>
2025-12-21 20:43:09 +0100

    Modo ESTRICTO

---
+++ modoEstricto/modoEstricto.js
@@ -0,0 +1,17 @@
+// Modo Estricto JS
+"use strict";
+
+
+ let x = 10
+
+console.log(x)
+
+miFuncion()
+
+function miFuncion(){
+    //"use strict"; Se puede usar al inicio de una funcion o al inicio de un programa
+
+     y = 15
+    console.log(y)
+
+}
