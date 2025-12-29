994b34d3b22aca50357a7158733f0ed09a5d85ac
Diegokh <diegoblanque04@outlook.com>
2025-12-29 22:13:13 +0100

    Manejo de errores

---
+++ manejoErrores/throw.js
@@ -0,0 +1,17 @@
+'use strict'
+let resultado = -7
+
+try{
+    if(isNaN(resultado)) throw 'No es un numero'
+    else if(resultado === '') throw 'Es cadena vacia'
+    else if(resultado >= 0) throw 'Valor positivo'
+    else if(resultado < 0) throw 'Valor negativo'
+}
+catch(error){
+    console.log(error)
+    //console.log(error.name)
+    //console.log(error.message)
+}
+finally{
+    console.log('Termina la revision de errores')
+}
---
+++ manejoErrores/trycatch.js
@@ -0,0 +1,16 @@
+// Manejo errores
+'use strict'
+
+
+try{
+   let x = 10
+  // miFuncion()
+}catch(error){
+console.log(error)
+}
+finally{ //Finally es opcional
+    console.log('Termina la revision de errores')
+}
+
+
+console.log('continuamos...')
