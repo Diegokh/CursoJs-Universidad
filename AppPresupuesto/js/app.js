91ba70bf3811bf3a8e7325ba50c7bf015785e65d
Diegokh <diegoblanque04@outlook.com>
2026-01-08 18:31:39 +0100

    App Presupuesto 1

---
+++ AppPresupuesto/js/app.js
@@ -0,0 +1,18 @@
+const ingresos = [
+    new Ingreso('Salario' ,2100),
+    new Ingreso('Venta coche',3000)
+];
+
+const egresos = [
+    new Egreso('Renta departamento', 500),
+    new Egreso('Ropa', 80)
+];
+
+let cargarApp = ()=>{
+    cargarCabecero();
+
+}
+
+let cargarCabecero = ()=>{
+    
+}
