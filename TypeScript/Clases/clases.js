b8e05ea3048cdd1208f2aa237c48c954006b3e99
Diegokh <diegoblanque04@outlook.com>
2026-01-11 19:11:59 +0100

    TypeScript 1

---
+++ TypeScript/Clases/clases.js
@@ -0,0 +1,17 @@
+class Persona {
+    nombre;
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+    getNombre() {
+        return this.nombre;
+    }
+    static metodoEstatico() {
+        return 10;
+    }
+}
+let persona1 = new Persona("Juan");
+console.log(persona1.getNombre());
+console.log(Persona.metodoEstatico());
+export {};
+//# sourceMappingURL=clases.js.map
---
+++ TypeScript/HolaMundoTypeScript/funciones.js
@@ -0,0 +1,16 @@
+let saludo = "HolaMundo desde TypeScript";
+saludo = "camion";
+let numero;
+numero = 10;
+let cualquiera;
+cualquiera = "cadena";
+cualquiera = 10;
+const PI_NUMERO = 3.14;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI_NUMERO);
+}
+saludar();
+export {};
+//# sourceMappingURL=funciones.js.map
